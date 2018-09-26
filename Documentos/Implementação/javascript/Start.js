6dab123bb0fb93c813078e8bcb88105b61a8af99
DESKTOP-SJ1RRGQ\Tomas <tomas.fmartins@gmail.com>
2018-09-26 23:55:29 +0100

    Pastas de código HTML/JAVASCRIPT/CSS

---
+++ Documentos/Implementação/javascript/Start.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function(){
+	window.addEventListener("load", main);
+}());
+
+function main(){
+	var blogin = document.getElementById("BLogin");
+	var bsign = document.getElementById("BSign");
+
+	blogin.addEventListener("click", bLoginPressed);
+	bsign.addEventListener("click", bSignPressed);
+
+	function bLoginPressed(){
+		window.location.href = "../html/Login.html"
+	}
+
+	function bSignPressed(){
+		window.location.href = "../html/SignUp.html"
+	}
+
+
+}

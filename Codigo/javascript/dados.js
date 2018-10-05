920c5eb60531f4ee3782916968870ed253604036
DESKTOP-SJ1RRGQ\Tomas <tomas.fmartins@gmail.com>
2018-10-05 01:04:02 +0100

    Correção de erros relativos aos dados do utilizador

--- Codigo/javascript/dados.js
+++ Codigo/javascript/dados.js
@@ -1,3 +1,7 @@
+const caract = "1234567890`~!@#$%^&*()-_+={[}]|:;'/?.>,<€ "+'"';
+const caract_2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()-_+={[}]|:;'/?.>,<€ "+'"';
+
+
 (function(){
 	window.addEventListener("load", main);
 }());
@@ -28,23 +32,58 @@ function main(){
 		else if(!photo.name.includes(".jpg") && !photo.name.includes(".png") && !photo.name.includes(".jpeg"))
 			alert("Photo must be JPEG or PNG format.");
 		else{
-			var reader = new FileReader();
-			reader.onloadend = function () {
-				var user = firebase.auth().currentUser;
-				var aux = user.displayName;
-				user.updateProfile({
-	  				displayName: aux+"|"+orcid.value+"|"+filiacao.value+"|"+unidade.value+"|"+interesses.value+"|"//+reader.result
-				}).then(function() {
-					window.location.href="../html/feed.html";
-				}).catch(function(error) {
-	  				console.log(error);
-				});
-       		}
-   			if(photo){
-      			reader.readAsDataURL(photo);
+
+			var controlo = 0;
+			
+			controlo = verificar(orcid.value, 1);
+			controlo = verificar(filiacao.value, 0);
+			controlo = verificar(unidade.value, 0);
+			controlo = verificar(interesses.value, 0);
+
+			if(controlo==0){
+				var reader = new FileReader();
+				reader.onloadend = function () {
+					var user = firebase.auth().currentUser;
+					var aux = user.displayName;
+					user.updateProfile({
+		  				displayName: aux+"|"+orcid.value+"|"+filiacao.value+"|"+unidade.value+"|"+interesses.value+"|"//+reader.result
+					}).then(function() {
+						window.location.href="../html/feed.html";
+					}).catch(function(error) {
+		  				console.log(error);
+					});
+	       		}
+	   			if(photo){
+	      			reader.readAsDataURL(photo);
+	    		}
     		}
 		}
 	}
 
-	
+}
+
+function verificar(check, flag){
+
+	if(flag==1){
+		for(var i=0; i< check.length ; i++){
+			for(var j=0; j< caract_2.length ; j++){
+				if(caract_2[j] == check[i]){
+					alert("ORCID number is invalid");
+					return 1;
+				}
+			}
+		}
+	}
+	else{
+		for(var i=0; i< check.length ; i++){
+			for(var j=0; j< caract.length ; j++){
+				if(caract[j] == check[i]){
+					alert("Names can not contain special characters.");
+					return 1;
+				}
+			}
+		}
+	}
+
+	return 0;
 }

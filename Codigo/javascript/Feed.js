c6fe024a9d14b48cc4a57580da6f4f4858ff21ea
Damiao Santos <joker.dss@gmail.com>
2018-11-15 14:48:16 +0000

    Implementação de Reddit (falta filtrar por interesses)

--- Codigo/javascript/Feed.js
+++ Codigo/javascript/Feed.js
@@ -1,13 +1,4 @@
-const api = 	["https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=afaebedbdb1e4e798277fd0b74955296", //Top bussiness Noticas nos US
-				"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=afaebedbdb1e4e798277fd0b74955296", //Top noticias de techcrunch
-				"https://newsapi.org/v2/everything?domains=wsj.com&apiKey=afaebedbdb1e4e798277fd0b74955296", //Todas as noticias do jornal wall street
-				"https://newsapi.org/v2/top-headlines?country=us&apiKey=afaebedbdb1e4e798277fd0b74955296",
-				"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=afaebedbdb1e4e798277fd0b74955296",
-				"https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=afaebedbdb1e4e798277fd0b74955296",
-				"https://newsapi.org/v2/top-headlines?q=trump&apiKey=afaebedbdb1e4e798277fd0b74955296",
-				"https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=afaebedbdb1e4e798277fd0b74955296",]; 
-
-//https://newsapi.org/v2/everything?q=bitcoin&from=2018-08-30&sortBy=publishedAt&apiKey=afaebedbdb1e4e798277fd0b74955296"
+var num;
 
 (function()
 {
@@ -15,65 +6,54 @@ const api = 	["https://newsapi.org/v2/top-headlines?country=us&category=business
 }());
 
 function main(){
+	const snoowrap = require('snoowrap');
+
+	const r = new snoowrap({
+	  userAgent: 'NewsFinder',
+	  clientId: '8PRDzdlBcEOdxg',
+	  clientSecret: 'RqQ5ZFLgo2-FyGX7KthAZTh-T3g',
+	  refreshToken: '95212774921-R6jorxu6Q99DpmeLerFO0S3_aoE'
+	});
+
 	var next = document.getElementById("Next");
 	var logout = document.getElementById("LogOut");
 	var nome = document.getElementById("Nome");
+	var interesses;
+	num = 0;
 	
 	firebase.auth().onAuthStateChanged(firebaseUser => {
 		if(firebaseUser){
 			nome.innerHTML = "USER: "+firebaseUser.displayName.split("|")[0]+" "+firebaseUser.displayName.split("|")[1];
+			interesses = firebaseUser.displayName.split("|")[5];
+			apresentaReddit(interesses, r);
 		}
 	});
 
-	var controlo = 0;
-
-	function carregarFeed(){
-		var requestURL = api[Math.floor(Math.random()*api.length)];
-		var request = new XMLHttpRequest();
-		request.open('GET', requestURL);
-		request.responseType = 'json';
-		request.send();
-		request.onload = function() {
-	  		var data = request.response;
-
-            if(data.status == "ok" && data.totalResults != 0){
-	  			var title = document.getElementById("title");
-	  			var author = document.getElementById("author");
-	  			var feed = document.getElementById("Feed");
-	  			var link = document.getElementById("link");
-		  		var num = Math.floor(Math.random() * data.articles.length);
-		  		if(data.articles[num].title != null)
-	  				title.innerHTML = " "+data.articles[num].title;
-	  			else
-	  				title.innerHTML = "Title: Unknown Title";
-	  			if(data.articles[num].author != null)
-	  				author.innerHTML = "Author: "+data.articles[num].author;
-	  			else
-	  				author.innerHTML = "Author: Unknown Author";
-	  			if(data.articles[num].description != null)
-	  				feed.innerHTML = " "+data.articles[num].description;
-	  			else
-	  				feed.innerHTML = "Description: Unknown Description";
-	  			link.setAttribute("href", data.articles[num].url);
-	  			controlo = 1;
-
-            }
-	  		if(controlo == 0){
-	  			carregarFeed();
-	  		}else{
-	  			controlo = 0;
-	  		}
-
-		}
-	}
-
-	carregarFeed();
-	next.addEventListener("click", carregarFeed);
+	next.addEventListener("click", mudarReddit);
 	logout.addEventListener("click", logingOut);
+
+	function mudarReddit(){
+		apresentaReddit(interesses, r);
+	}
 
 	function logingOut(){
 		firebase.auth().signOut();
 		window.location.href = "../html/index.html";
 	}
 
+}
+
+function apresentaReddit(interesses, r){
+	//falta um limitador no NUM, falta interesses, controlo se interesse existe mesmo || Tem de ser tudo junto
+	var title = document.getElementById("title");
+	var link = document.getElementById("link");
+
+	r.getHot().map(post => post).then(show);
+	
+	function show(array){
+		title.innerHTML = "Reddit: "+(array[num].title);
+		var referencia = 'www.reddit.com'+array[num].permalink
+		link.setAttribute('href', referencia);
+		num++;
+	}
 }
